24914fea4cdbba7789fa23b65f93731304750ec5
dylnb <dbumford@gmail.com>
2016-05-01 19:26:57 +0000

    change comonad tree annotation example to log types

--- type-driven/comonad-categorial-typecheck.hs
+++ type-driven/comonad-categorial-typecheck.hs
@@ -1,33 +1,54 @@
 
--- from sigfpe
--- http://blog.sigfpe.com/2006/11/variable-substitution-gives.html
+import Control.Monad (liftM2, join)
 
 class Comonad w where
-   counit :: w a -> a
-   cobind :: (w a -> b) -> w a -> w b
+  counit :: w a -> a
+  cobind :: (w a -> b) -> w a -> w b
 
-data Parts' a = A [Parts a] | S String deriving (Eq,Show)
-data Parts a = P (Parts' a,a) deriving (Eq,Show)
+data Cat = E | T | Cat :\: Cat | Cat :/: Cat deriving (Show, Eq)
+data Tree a = Leaf a | Branch a (Tree a) (Tree a)
 
-instance Comonad Parts where
-   counit (P (_,a)) = a
-   cobind f z = case z of
-       P (S s,_) -> P (S s,f z)
-       P (A l,_) -> P (A $ map (cobind f) l,f z)
+instance Show a => Show (Tree a) where
+  show = unlines . pp
+    where pp (Leaf x) = [show x]
+          pp (Branch x left right) = show x : pp_subtree left right
+          pp_subtree left right =
+             pad "+- " "|  " (pp right) ++ pad "`- " "   " (pp left)
+          pad first rest = zipWith (++) (first : repeat rest)
 
+instance Comonad Tree where
+  counit (Leaf x) = x
+  counit (Branch x _ _) = x
 
-total :: Num a => Parts a -> a
-total (P (S _,n)) = n
-total (P (A x,_)) = sum $ map total x
+  cobind f z = case z of
+    Leaf _ -> Leaf (f z)
+    Branch _ t1 t2 -> Branch (f z) (cobind f t1) (cobind f t2)
 
-lwing, rwing, cockpit, fuselage, body, plane :: Parts Int
-lwing = P (S "left wing",100)
-rwing = P (S "right wing",100)
-cockpit = P (S "cockpit",200)
-fuselage = P (S "fuselage",200)
-body = P (A [fuselage,cockpit],undefined)
-plane = P (A [lwing,rwing,body],undefined)
+cat :: Tree Cat -> Maybe Cat
+cat (Leaf c) = Just c
+cat (Branch _ t1 t2) = join $ liftM2 apply (cat t1) (cat t2)
+  where apply (f :/: x) y | x == y  = Just f
+        apply y (x :\: f) | x == y  = Just f 
+        apply _ _                   = Nothing                    
 
-test4 :: Parts Int
-test4 = cobind total plane
+leaf :: a -> Tree a
+leaf = Leaf
+branch :: Tree a -> Tree a -> Tree a
+branch = Branch undefined
 
+gq, ent, vrb, hfvrb, goodTree, goodHFTree, badTree :: Tree Cat
+gq  = leaf (T:/:(E:\:T))
+ent = leaf E
+vrb = leaf ((E:\:T):/:E)
+hfvrb = leaf (E:\:(E:\:T))
+goodTree = branch gq (branch vrb ent)
+goodHFTree = branch gq (branch ent hfvrb)
+badTree  = branch ent (branch vrb gq) 
+
+testGood, testGoodHF, testBad :: Tree (Maybe Cat)
+testGood   = cobind cat goodHFTree
+testGoodHF = cobind cat goodTree
+testBad    = cobind cat badTree
+
+main :: IO ()
+main = print testGood >> print testGoodHF >> print testBad
